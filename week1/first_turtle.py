c215011a7bf7567b7ab73488b3708cd2e0433977
Ali Dahud <alidahud@gmail.com>
2021-05-25 17:00:05 +0200

    Updated first turte

--- week1/first_turtle.py
+++ week1/first_turtle.py
@@ -1,8 +1,19 @@
 import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+tess = turtle.Turtle()
+tess.color("blue")
+tess.shape("turtle")
 
-wn=turtle.Screen()
-ella=turtle.Turtle()
-ella.right(90)
-ella.forward(150)
-ella.left(90)
-ella.forward(75)
+
+tess.up()                     # this is new
+for _ in range(10):    # start with size = 5 and grow by 2
+    tess.fd(50)# and turn her
+    tess.stamp()                # leave an impression on the canvas in a for loo    p to not write it 10 times
+    tess.forward(-50)          # move tess along
+    tess.right(36)
+
+
+
+
+wn.exitonclick()
