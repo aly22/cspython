4036bb8849f009d68390945fe81a6e8b3613a4ab
Ali Dahud <alidahud@gmail.com>
2021-05-29 17:39:46 +0200

    --

---
+++ week4/trying.py
@@ -0,0 +1,6 @@
+winners = ['Alice Munro', 'Alvin E. Roth', 'Kazuo Ishiguro', 'Malala Yousafzai', 'Rainer Weiss', 'Youyou Tu']
+z_winners=winners[:]
+z_winners.sort()
+z_winners.reverse()
+print(z_winners)
+
