0c6f4c47f21d1a746f68998cf2a14885eabca5c2
Ali Dahud <alidahud@gmail.com>
2021-05-31 15:54:19 +0200

    exercise #2 and #3

--- week5_csv_txt_files/week5_exs.py
+++ week5_csv_txt_files/week5_exs.py
@@ -1,10 +1,27 @@
-
-#1
-path='studentdata.txt'
-with open(path,'r') as file:
+# 1
+path = 'studentdata.txt'
+with open(path, 'r') as file:
     for row in file:
-        student_score=row.strip().split()
+        student_score = row.strip().split()
 
-        if len(student_score[1:])>6:
+        if len(student_score[1:]) > 6:
             print(student_score[0])
 
+# 2
+path = "travel_plans.txt"
+destination = []
+with open(path, "r") as file:
+    for row in file:
+        travel = row.split()
+        if travel[0][-1] == ":":
+            destination.append(row)
+# 3
+path = "emotion_words.txt"
+j_emotions = []
+with open(path, "r") as file:
+    file_ = file.read()
+    file_lst = file_.split()
+    for word in file_lst:
+        if word[0] == "j":
+            j_emotions.append(word)
+print(j_emotions)
