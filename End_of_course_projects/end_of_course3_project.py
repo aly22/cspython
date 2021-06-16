8878d7e8cb3158dfad834e7929c6eece646e76ba
Ali Dahud <alidahud@gmail.com>
2021-06-16 09:16:37 +0200

    reformatted the code so it looks cleaner.

--- End_of_course_projects/end_of_course3_project.py
+++ End_of_course_projects/end_of_course3_project.py
@@ -26,6 +26,8 @@ def extract_movie_titles(movies_json):
         names.append(movie["Name"])
 
     return names
+
+
 # def filtery(lst):
 #     for title in extract_movie_titles(get_movies_from_tastedive(lst)):
 #         started on filtering the movies but then realised that it's the same as the list comprehension --> can't refer to itself
@@ -51,25 +53,30 @@ def get_movie_data(movie):
 
 
 def get_movie_rating(jsondata):
-    RT_rating=0
+    RT_rating = 0
     for rating in jsondata["Ratings"]:
-        if"Rotten Tomatoes" in rating["Source"]:
-            RT_rating+=int(rating["Value"][:-1])
+        if "Rotten Tomatoes" in rating["Source"]:
+            RT_rating += int(rating["Value"][:-1])
             return RT_rating
     return 0
 
+
 def get_sorted_recommendations(lst_of_movies):
-    lst_of_recommendations=[]
-    ret_lst=[]
-    related_movies=get_related_titles(lst_of_movies)
+    lst_of_recommendations = []
+    ret_lst = []
+    related_movies = get_related_titles(lst_of_movies)
     for movie in related_movies:
-        rating=get_movie_rating(get_movie_data(movie))
-        lst_of_recommendations.append([movie,rating])
-    for recommendation in sorted(lst_of_recommendations,key=lambda x:(x[1],x[0]),reverse=True):
+        rating = get_movie_rating(get_movie_data(movie))
+        lst_of_recommendations.append([movie, rating])
+    for recommendation in sorted(lst_of_recommendations,
+                                 key=lambda x: (x[1], x[0]), reverse=True):
         ret_lst.append(recommendation)
     return [movie[0] for movie in ret_lst]
 
-print(get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"]))
+
+# print(get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"]))
+
+
 # print(get_related_titles(["Black Panther", "Captain Marvel"]))
 # cpt_marv=(get_movies_from_tastedive("Captain Marvel"))
 # jsonobj=json.dumps(cpt_marv,indent=2)
@@ -86,8 +93,8 @@ def read_from_file(path):
         data = json.load(file)
         return json.loads(data)
 
-
 # movies = (read_from_file("movies.json"))
 # movies = json.dumps(movies, indent=2)
 # print(movies)
 # print(extract_movie_titles(get_movies_from_tastedive("Black Panther")))
+
