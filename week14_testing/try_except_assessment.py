b29a0ef1de1adfd64e17ecd49887d58aa348280f
Ali Dahud <alidahud@gmail.com>
2021-06-18 16:36:04 +0200

    End of course 4 project (bad)

--- week14_testing/try_except_assessment.py
+++ week14_testing/try_except_assessment.py
@@ -110,7 +110,7 @@ myString = 'Hello, World! 123'
 
 print(myString.upper()) # HELLO, WORLD! 123
 print(myString.lower()) # hello, world! 123
-    print(myString.count('l')) # 3
+print(myString.count('l')) # 3
 
 s = 'python is pythonic'
 print(s.count('python')) # 2
