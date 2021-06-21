924eb3bcef6bfafb31e58d9cc2e93eee384d7c18
Ali Dahud <alidahud@gmail.com>
2021-06-21 17:15:15 +0200

    Cleaned up code!!

--- End_of_course_projects/9photosInOneProject.py
+++ End_of_course_projects/9photosInOneProject.py
@@ -1,10 +1,4 @@
-path = r"C:\Users\ALI-Gsus\Downloads\PsViragokSzurkeKicsi.jpg"
-# image.getpixel# First, lets import all of the library functions we need
-import PIL
-import numpy as np
-from PIL import Image
-from PIL import ImageFilter
-from PIL import ImageFont, ImageDraw
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
 # from IPython.display import display
 
@@ -15,18 +9,30 @@ image = Image.open(file).convert('RGBA')
 
 def pixel_getter(image):
     images = []
+    font = ImageFont.truetype(
+        r"F:\pythonalapok\pythonka\cspython\End_of_course_projects\fanwood-webfont.ttf",
+        50)
 
     for i in range(3):
-        # i=0
+
         cnt = 0.1
         for j in range(3):
-            pixels = image.load()
-            # j=0
+            newim2 = Image.new(image.mode, image.size)
+            draw = ImageDraw.Draw(newim2)
+            if i==0:
+                draw.text((15, 400), "channel {} intensity {}".format(cnt, i),
+                      fill=(int(255*cnt), 255, 255), font=font)
+            elif i==1:
+                draw.text((15, 400), "channel {} intensity {}".format(cnt, i),
+                          fill=(255, int(255*cnt), 255), font=font)
+            else:
+                draw.text((15, 400), "channel {} intensity {}".format(cnt, i),
+                          fill=(255, 255, int(255*cnt)), font=font)
+
             newim = image.filter(ImageFilter.BoxBlur(0))
             for x in range(image.width):
                 for y in range(image.height):
-                    # for color in image.getdata():
-                    #     images.append((color))
+
                     r = (image.getpixel((x, y)))[0]
                     g = (image.getpixel((x, y)))[1]
                     b = (image.getpixel((x, y)))[2]
@@ -37,69 +43,35 @@ def pixel_getter(image):
                         newim.putpixel((x, y), (r, int(g * cnt), b))
                     else:
                         newim.putpixel((x, y), (r, g, int(cnt * b)))
+            blended_img = Image.composite(newim2, newim, newim2)
             cnt += 0.4
-            images.append(newim)
+            images.append(blended_img)
     return (images)
-
-
-def image_changer(lst):
-    newimdatas = []
-    for i in range(3):
-        cnt = 0.1
-
-        for j in range(3):
-            newimdata = []
-            for color in lst:
-                r, g, b = color[0], color[1], color[2]
-                if i == 0:
-                    newimdata.append((int(r * cnt), g, b))
-                elif i == 1:
-                    newimdata.append((r, int(g * cnt), b))
-                else:
-                    newimdata.append((r, g, int(b * cnt)))
-            newimdatas.append(newimdata)
-            cnt += .4
-
-    return newimdatas
 
 
 images = pixel_getter(image)
 
 
 def put_images(lst, image):
-    font=ImageFont.truetype(r"F:\pythonalapok\pythonka\cspython\End_of_course_projects\fanwood-webfont.ttf",50)
-
     newim = Image.new(image.mode, (image.width * 3, image.height * 3))
     x = 0
     y = 0
-    i=0.1
-    cnt=0
-    lst2=[]
-    # for img in lst:
-
-
-        # blended_img=Image.merge("RGB",(newim2,img))
-        # blended_img=Image.composite(newim2,,)
-        # lst2.append(blended_img)
-        # blended_img.show()
+    i = 0.1
+    cnt = 0
     for img in lst:
-        newim2=Image.new(img.mode,img.size)
-        draw=ImageDraw.Draw(newim2)
-        draw.text((15,400),"channel {} intensity {}".format(cnt,i),fill=(255,255,255),font=font)
-        blended_img=Image.composite(newim2,img,newim2)
-        newim.paste(blended_img, (x, y))
 
+        newim.paste(img, (x, y))
 
         print(x, y)  # think Ali think
         print(i)
         if x + image.width == newim.width:
             x = 0
             y += image.height
-            i=0.1
-            cnt+=1
+            i = 0.1
+            cnt += 1
         else:
             x += image.width
-            i+=.4
+            i += .4
 
     return newim
 
